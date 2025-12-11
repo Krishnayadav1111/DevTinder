a5dd3b2e9df0edd87968925620d010359edc7d2c
Krishnayadav1111 <krishnantv1111@gmail.com>
2025-12-11 22:41:50 +0530

    test done

--- src/app.js
+++ src/app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/hello/2", (req, res) => {
+  res.send('Hello hello! hgfhgf gfhgfhf');
+});
+
+//this will match all the HTTP methods (GET, POST, PUT, DELETE, etc.) for the /hello route
+app.use("/hello", (req, res) => {
+  res.send('Hello hello!');
+});
+
 app.use("/", (req, res) => {
   res.send('Hello World! 45');
 });
 
-app.use("/hello", (req, res) => {
-  res.send('Hello hello!');
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
