99c4cf0fb63c6eb17fc1ce1f26b197764069e8ba
Krishnayadav1111 <krishnantv1111@gmail.com>
2025-12-12 18:59:58 +0530

    signup done

--- src/app.js
+++ src/app.js
@@ -3,19 +3,23 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/database');
 const User = require('./models/user');
-
+app.use(express.json());
 
 
 app.post('/signup', (req, res) => {
  
-  const userObj={
-    firstName: "Virat",
-    lastName: "kolhi",
-    emailId: "jsaonh@gmail.com",
-    password: "johns@123",
-    age: 25,}
-    // Create a new user instance and save it to the database
-    const user = new User(userObj);
+  console.log(req.body,"reqbody");
+  const userObj=req.body;
+  // Create a new user instance and save it to the database
+  const user = new User(userObj);
+  // const userObj={
+  //   firstName: "Virat",
+  //   lastName: "kolhi",
+  //   emailId: "jsaonh@gmail.com",
+  //   password: "johns@123",
+  //   age: 25,}
+  //   // Create a new user instance and save it to the database
+  //   const user = new User(userObj);
     user.save().then(() => {
       console.log("User saved successfully");
     }).catch((err) => {
